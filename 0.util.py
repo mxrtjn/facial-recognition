b37af3fc71148e68121db1fa4b552c1e3b1f139c
Felipe <martin.pacora@gmail.com>
2018-07-03 21:16:29 -0500

    adding code

---
+++ 0.util.py
@@ -0,0 +1,27 @@
+import os, sys
+
+dataSetPath = os.getcwd()+'/dataSet2'
+
+#print("The dir is: %s"%os.listdir(dataSetPath))
+counter = 0
+counterFile = 0
+for fold in os.listdir(dataSetPath):
+    if not os.path.isdir(os.path.join(dataSetPath, fold)):#if not is a folder
+        continue
+    counter = counter + 1
+    counterFile = 0
+    #print(fold)
+    for file in os.listdir(os.path.join(dataSetPath, fold)):
+        if not os.path.isdir(os.path.join(dataSetPath, fold, file)):
+            counterFile = counterFile + 1
+            os.rename(os.path.join(dataSetPath, fold, file), os.path.join(dataSetPath, fold, fold + '-' + str(counter) + '.' + str(counterFile) + '.jpg'))
+    os.rename(os.path.join(dataSetPath, fold), os.path.join(dataSetPath, str(counter) + '-' + fold))
+    #        print('   ' + file)
+    #onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+    #if counter > 1:
+    #    break
+
+
+
+#os.rename(dataSetPath+'/Aaron_Eckhart',dataSetPath+'/1.Aaron_Eckhart')
---
+++ 3.detector.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()#cv2.createLBPHFaceRecognizer()
+recognizer.read('trainer/trainer.yml')#recognizer.load('trainer/trainer.yml')
+cascadePath = "Classifiers/face.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+
+cam = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_SIMPLEX #font = cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 1, 1) #Creates a font
+
+while True:
+    ret, im = cam.read()
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
+    for(x,y,w,h) in faces:
+        nbr_predicted = ''
+        cv2.rectangle(im, (x - 50, y - 50), (x + w + 50, y + h + 50), (225, 0, 0), 2)
+        nbr_predicted, conf = recognizer.predict(gray[y:y+h,x:x+w])
+
+        print(nbr_predicted)
+        if conf < 50:
+            if nbr_predicted == 1:
+                nbr_predicted = 'User 1'
+            elif nbr_predicted == 2:
+                nbr_predicted = 'Felipe'
+        else:
+            nbr_predicted = "Unknown"
+        cv2.putText(im, str(nbr_predicted)+"--"+str(conf), (x, y+h), font, 1, (255, 0, 0)) #Draw the text
+        cv2.imshow('im', im)
+        #cv2.waitKey(10)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+cam.release()
+cv2.destroyAllWindows()
